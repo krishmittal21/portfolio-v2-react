34d037ac40129759fe76742f670ec15e6e89efdf
Krish Mittal <114004042+krishmittal21@users.noreply.github.com>
2023-06-25 22:01:24 +0530

    about me

---
+++ src/about/subheadingData.js
@@ -0,0 +1,61 @@
+const subheadingsData = {
+    1: [
+        {
+            title: "About Me",
+            content: (
+            <>
+                <p>
+                A product manager based out of delhi aspiring to be a cloud architect and a developer
+                </p>
+            </>
+            ),
+        },
+    ],
+    2: [
+        {
+            title: " BITS Pilani : Bsc Computer Science 2025 ",
+            content: (
+            <>
+                <p>
+                
+                </p>
+            </>
+            ),
+        },
+        {
+            title: " Harvard : CS50 Python 2023 ",
+            content: (
+            <>
+                <p>
+                
+                </p>
+            </>
+            ),
+        },
+        {
+            title: " QubitbyQubit : Intro to Quantum Computing 2022 ",
+            content: (
+            <>
+                <p>
+                
+                </p>
+            </>
+            ),
+        },
+    ],
+    3: [
+        {
+            title: "QuantumGrad Product Manager 2022-present",
+            content: (
+            <>
+                <p>
+                
+
+                </p>
+            </>
+            ),
+        },
+        
+    ],
+};
+export default subheadingsData;
