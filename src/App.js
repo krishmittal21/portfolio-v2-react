0475b2c888db31f0e993e3255a81033e643c05e5
Krish Mittal <114004042+krishmittal21@users.noreply.github.com>
2023-06-25 13:56:42 +0530

    navbar styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./navBar/NavBar.js";
-import Home from "./home/Home.js";
+import About from "./about/About.js";
 import Skills from "./skills/Skills.js";
 import Projects from "./projects/Projects.js";
 import Contact from "./contact/Contact.js";
+import "./styles/app.css";
 const App=()=> {
   return (
     <>
     <Router>
       <NavBar/>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<About/>}/>
         <Route path="/skills" element={<Skills/>}/>
         <Route path="/projects" element={<Projects/>}/>
         <Route path="/contact" element={<Contact/>}/>
