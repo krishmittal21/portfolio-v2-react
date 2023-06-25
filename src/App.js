6878ce182f4dc6d3041d1fdd2823098f33c9c79f
Krish Mittal <114004042+krishmittal21@users.noreply.github.com>
2023-06-25 14:42:44 +0530

    mystats

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Projects from "./projects/Projects.js";
 import Contact from "./contact/Contact.js";
 import "./styles/app.css";
 import Background from "./background/Background.js";
+import PlayerStats from "./playerStats/PlayerStats.js";
 const App=()=> {
   return (
     <>
@@ -19,6 +20,7 @@ const App=()=> {
         <Route path="/projects" element={<Projects/>}/>
         <Route path="/contact" element={<Contact/>}/>
       </Routes>
+      <PlayerStats/>
     </Router>
     </>
   );
