46dbc82d9602938cb70e651b668df5fb78e745cf
Krish Mittal <114004042+krishmittal21@users.noreply.github.com>
2023-06-25 13:36:41 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./navBar/NavBar.js";
+import Home from "./home/Home.js";
+import Skills from "./skills/Skills.js";
+import Projects from "./projects/Projects.js";
+import Contact from "./contact/Contact.js";
 const App=()=> {
   return (
     <>
+    <Router>
       <NavBar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/skills" element={<Skills/>}/>
+        <Route path="/projects" element={<Projects/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
+    </Router>
     </>
   );
 }
--- src/navBar/NavBar.js
+++ src/navBar/NavBar.js
@@ -1,9 +1,74 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Link, useLocation } from "react-router-dom";
+import astronautHelmet from "../assests/astronaut-helmet.png";
+import deadEye from "../assests/dead-eye.png";
+import stack from "../assests/stack.png";
+import envelope from "../assests/envelope.png";
+import "../styles/NavBar.css";
 
-export default class NavBar extends Component {
-    render() {
-        return (
-            <div>NavBar</div>
-        )
+export default function NavBar() {
+    const location = useLocation();
+
+    const getNavPositionClass = () => {
+        switch (location.pathname) {
+        case "/":
+            return "nav-about";
+        case "/skills":
+            return "nav-skills";
+        case "/projects":
+            return "nav-projects";
+        case "/contact":
+            return "nav-contact";
+        default:
+            return "";
     }
+};
+
+const getPageTitle = () => {
+        switch (location.pathname) {
+        case "/":
+            return "ABOUT";
+        case "/skills":
+            return "SKILLS";
+        case "/projects":
+            return "PROJECTS";
+        case "/contact":
+            return "CONTACT";
+        default:
+            return "";
+    }
+};
+
+const navPositionClass = getNavPositionClass();
+const pageTitle = getPageTitle();
+
+const isCurrentPage = (navClass) => {
+    return navClass === navPositionClass;
+};
+
+const renderNavLink = (to, imgSrc, altText, navClass) => {
+    const isCurrent = isCurrentPage(navClass);
+    const linkClass = isCurrent ? "nav-link current" : "nav-link";
+
+    return (
+        <Link to={to} className={linkClass}>
+            <img src={imgSrc} alt={altText} />
+            {isCurrent && <h1 className="page-title">{pageTitle}</h1>}
+        </Link>
+    );
+};
+
+return (
+    <nav className={`nav ${navPositionClass}`}>
+        {renderNavLink(
+            "/",
+            astronautHelmet,
+            "astronaut helmet icon",
+            "nav-about"
+        )}
+        {renderNavLink("/skills", deadEye, "deadEye icon", "nav-skills")}
+        {renderNavLink("/projects", stack, "stack icon", "nav-projects")}
+        {renderNavLink("contact", envelope, "envelope icon", "nav-contact")}
+    </nav>
+    );
 }
