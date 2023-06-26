571db696d2787aad62099de50b67608769fdc6ad
Krish Mittal <114004042+krishmittal21@users.noreply.github.com>
2023-06-26 15:15:07 +0530

    Update PlayerStats.js

--- src/playerStats/PlayerStats.js
+++ src/playerStats/PlayerStats.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "../styles/playerStats.css";
-
+import {AiFillTwitterCircle, AiFillLinkedin, AiFillGithub,  AiFillMail} from "react-icons/ai";
 const PlayerStats = () => {
     return (
         <div id="playerStats">
-        <h1>Krish Mittal
+        <h1>
+        <div className="flex items-center gap-2" >
+        <span>Krish Mittal</span>
+            <a href="https://github.com/krishmittal21" target="_blank">
+                <AiFillGithub />
+            </a>
+            <a href="https://twitter.com/krishmittal_" target="_blank">
+                <AiFillTwitterCircle/>
+            </a>
+            <a href="https://www.linkedin.com/in/krishmittal/" target="_blank">
+                <AiFillLinkedin/>
+            </a>
+            <a href="mailto:krishmittal212005@gmail.com">
+                <AiFillMail/>
+            </a>
+            </div>
             <br />
             LEVEL 18</h1>
         <div id="playerStats__lines">
