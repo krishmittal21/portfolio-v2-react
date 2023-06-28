a14b2cf95f78769d8a88c19219354436ded8ce1d
Krish Mittal <114004042+krishmittal21@users.noreply.github.com>
2023-06-28 16:06:36 +0530

    Update PlayerStats.js

--- src/playerStats/PlayerStats.js
+++ src/playerStats/PlayerStats.js
@@ -1,24 +1,13 @@
 import React from "react";
 import "../styles/playerStats.css";
-import {AiFillTwitterCircle, AiFillLinkedin, AiFillGithub,  AiFillMail} from "react-icons/ai";
+import {AiFillTwitterCircle, AiFillLinkedin, AiFillGithub,  AiFillMail, AiFillBook} from "react-icons/ai";
 const PlayerStats = () => {
     return (
         <div id="playerStats">
         <h1>
         <div className="flex items-center gap-2" >
         <span>Krish Mittal</span>
-            <a href="https://github.com/krishmittal21" target="_blank">
-                <AiFillGithub />
-            </a>
-            <a href="https://twitter.com/krishmittal_" target="_blank">
-                <AiFillTwitterCircle/>
-            </a>
-            <a href="https://www.linkedin.com/in/krishmittal/" target="_blank">
-                <AiFillLinkedin/>
-            </a>
-            <a href="mailto:krishmittal212005@gmail.com">
-                <AiFillMail/>
-            </a>
+            
             </div>
             
             LEVEL 18</h1>
@@ -30,6 +19,23 @@ const PlayerStats = () => {
             
             Product Manager
         </h2>
+        <div className="flex  justify-start gap-1">
+        <a href="https://github.com/krishmittal21" target="_blank">
+                <AiFillGithub />
+            </a>
+            <a href="https://twitter.com/krishmittal_" target="_blank">
+                <AiFillTwitterCircle/>
+            </a>
+            <a href="https://www.linkedin.com/in/krishmittal/" target="_blank">
+                <AiFillLinkedin/>
+            </a>
+            <a href="mailto:krishmittal212005@gmail.com">
+                <AiFillMail/>
+            </a>
+            <a href="https://docs.google.com/document/d/1t_RGrKz5V0mWJ0wzWueFE368S37o03K3Ni23_nwB0JQ/edit" target="_blank">
+                <AiFillBook/>
+            </a>
+        </div>
         </div>
     );
 };
